343bb163608f6e52424a29a84f7990419c9fcb18
pltoledo <pedroltoledo@gmail.com>
2023-12-02 10:46:38 -0300

    Fix chromadb version and initialization with pysqlite3

--- streamlit_chroma_connection/__init__.py
+++ streamlit_chroma_connection/__init__.py
@@ -1,3 +1,8 @@
+__import__("pysqlite3")  # noqa: E402
+import sys  # noqa: E402
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")  # noqa: E402
+
 from .chroma_connection import ChromaDBConnection
 
 __all__ = ["ChromaDBConnection"]
